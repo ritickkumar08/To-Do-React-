6f885b081a0dff391efcfd65b817d24db549d443
ritickkumar <ritickkumar08@gmail.com>
2026-01-08 17:24:11 +0530

    added comments about the todoList component usage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 
-
 function Header() {
     return(
         <h1>Header</h1>
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -3,7 +3,13 @@ import ToDoItem from './ToDoItem'
 
 /**
  * @returns the UI to display the whole list of todos enterred by the user
+ * it recieves :
+ *  -> todos : an array of tasks/todos
+ *  -> onToggel, onEdit, onDelete, onSave
  * 
+ * its responsible for:
+ *  -> display all todo items using map
+ *  -> pass all the todos and the utility functions
  */
 
 function ToDoList({todos, onToggle, onEdit, onSave, onDelete}) {
