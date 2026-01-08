d98b097b8aeaafe2558ac37e0bcfb2a4984b4db0
ritickkumar <ritickkumar08@gmail.com>
2026-01-08 22:01:32 +0530

    huhh

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
         <div>
             <div className='flex items-center justify-around bg-red-500 shadow-md shadow-red-300 h-30 w-[80vw] rounded-lg'>
                 <img className='w-2xs p-0' src={logoForLight} alt="logo" />
-                <p className='text-3xl'>keep your to do list</p>
+                <p className='text-3xl'>Things You Forgot to Forget 😎</p>
             </div>
         </div>
     )
